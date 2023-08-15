d8dfffdf698d9b95925bf93cde2430fc12a14c57
majdiShomali <majdi.shomali.1997@gmail.com>
2023-08-16 01:46:30 +0300

    edit

--- middleware/Protected.js
+++ middleware/Protected.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const SECRETKEY = process.env.SECRETKEY;
 module.exports = (req,res,next)=>{
   const token = req.headers.authorization.trim();
+  console.log(token);
   if (!token) {
     return res.status(401).json({ message: 'No token provided.' });
   }
