ed5b748df27c1f89ad79b36f0b811afe4c0e3a80
majdiShomali <majdi.shomali.1997@gmail.com>
2023-08-26 13:35:00 +0300

    edit upoad to firebase

--- routes/itemCategoryRouter.js
+++ routes/itemCategoryRouter.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 const itemCategoryController = require("../controllers/itemCategoryController");
 const upload = require("../middleware/handleImage")
+const uploadMiddleware = require('../middleware/uploadMiddleware'); // Correct import
 
 router.get("/api/allItems", itemCategoryController.allItems);
 router.get("/api/OneItem/:id", itemCategoryController.OneItem);
 router.get("/api/ProviderItems/:id", itemCategoryController.ProviderItems);
 
-router.post("/api/items",upload.single("image"), itemCategoryController.addItem);
+router.post("/api/items",uploadMiddleware, itemCategoryController.addItem);
 
 router.put("/api/updateItemData/:id", itemCategoryController.updateItemData);
-router.put("/api/updateItemImage/:id",upload.single("image"), itemCategoryController.updateItemImage);
+router.put("/api/updateItemImage/:id",uploadMiddleware, itemCategoryController.updateItemImage);
 
 
 // router.put("/api/updateProductColor/:id",upload.single("image"), itemCategoryController.updateProductColor);
